62a58451b88c2063f92ad886fbe35b23a83ab5ac
unknown <Aleksei_Ivshin@EPRUIZHW0199.moscow.epam.com>
2014-12-12 18:20:48 +0400

    Add publisher

--- src/main/java/ru/udm/app/AppProducer.java
+++ src/main/java/ru/udm/app/AppProducer.java
@@ -1,34 +1,75 @@
 package ru.udm.app;
 
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import ru.udm.app.helpers.MessageBundle;
+import ru.udm.jms.publishers.Publisher;
 import ru.udm.jms.queue.Producer;
 
 public class AppProducer {
 
+	private static Logger LOGGER = LoggerFactory
+			.getLogger(AppProducer.class);
+	
+	private ApplicationContext context;
+
 	public static void main(String[] args) {
-		new AppProducer().init();
+		director(new AppProducer());
 	}
 
-	public void init() {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
-		Producer producer = ctx.getBean(Producer.class);
-		MessageBundle messageBundle = ctx.getBean(MessageBundle.class);
-		String message;
-		while ((message = messageBundle.getNext()) != null) {
-			producer.send(message);
-			try {
-				Thread.sleep(3000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-				break;
+	public static void director(AppProducer app) {
+		app.init();
+		app.director();
+	}
+
+	private void director() {
+		Producer producer = context.getBean(Producer.class);
+		Publisher publisher = context.getBean(Publisher.class);
+		MessageBundle messageBundle = context.getBean(MessageBundle.class);
+
+		try (Scanner sc = new Scanner(System.in)) {
+			String line = "";
+			while ((line = sc.next()) != null && !line.isEmpty()) {
+				String[] command = line.split(":", 2);
+				LOGGER.debug("Command:{}",command);
+				switch (command[0]) {
+				case "publish":
+					publisher.publish(command[1]);
+					break;
+				case "send":
+					producer.send(command[1]);
+					break;
+				case "sendFromBundle":
+					String messageFromBundle;
+					if((messageFromBundle = messageBundle.getNext())!=null){
+						producer.send(messageFromBundle);
+					}
+					break;
+				case "exit":
+					close();
+					System.exit(1);
+				default:
+					break;
+				}
 			}
 		}
+	}
 
-		((AbstractApplicationContext) ctx).close();
+	private void init() {
+		context = new ClassPathXmlApplicationContext("beans.xml");
+
+	}
+
+	private void close() {
+		if (context != null) {
+			((AbstractApplicationContext) context).close();
+		}
 	}
 
 }
--- src/main/java/ru/udm/jms/publishers/Publisher.java
+++ src/main/java/ru/udm/jms/publishers/Publisher.java
@@ -1,9 +1,9 @@
-package ru.udm.jms.queue;
+package ru.udm.jms.publishers;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.Queue;
 import javax.jms.Session;
+import javax.jms.Topic;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,28 +14,25 @@ import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Producer {
+public class Publisher {
 	private static Logger LOGGER = LoggerFactory
-			.getLogger(Producer.class);
+			.getLogger(Publisher.class);
 
 	@Autowired
-	@Qualifier("jmsProducerTemplate")
+	@Qualifier("jmsTopicTemplate")
 	JmsTemplate jmsTemplate;
 
 	@Autowired
-	@Qualifier("defaultDestination")
-	Queue destination;
-
-	public void send(final String messageText) {
+	@Qualifier("topicDestination")
+	Topic destination;
+	
+	public void publish(final String message){
 		this.jmsTemplate.send(destination, new MessageCreator() {
-
+			
 			@Override
 			public Message createMessage(Session session) throws JMSException {
-
-				LOGGER.debug("publish to queue '{}' message '{}'",destination.getQueueName(),messageText);
-				return session.createTextMessage(this.getClass().getName()
-						+ " send message '" + messageText + "' timestamp:"
-						+ System.currentTimeMillis());
+				LOGGER.debug("publish to topic '{}' message '{}'",destination.getTopicName(),message);
+				return session.createTextMessage(message);
 			}
 		});
 	}
